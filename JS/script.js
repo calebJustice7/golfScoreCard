6b8c5343ec695519eb724d258f6f09512a1bcafa
Caleb Justice <calebjustice@Calebs-MacBook-Pro.local>
2019-11-07 07:44:09 -0700

    choosing tee type works

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+let courseId = [18300, 11819, 19002];
+let selectedCourse;
+
 function getCourseList() {
     return new Promise((resolve, reject) => {
         let xhttp = new XMLHttpRequest();
@@ -18,14 +21,16 @@ getCourseList().then(courses => {
 
 getCourseList();
 
-function getCourseInfo(id) {
+function getCourseInfo(id, event) {
     if(id) {
         return new Promise((resolve, reject) => {
             let x = new XMLHttpRequest();
             x.onreadystatechange = function () {
                 if (this.status === 200 && this.readyState === 4) {
                     const COURSE = JSON.parse(this.responseText);
-                    getThisCourse(COURSE);
+                    selectedCourse = COURSE;
+                    console.log(selectedCourse);
+                    getThisCourse(COURSE, id, event);
                 }
             };
             x.open("GET", `https://golf-courses-api.herokuapp.com/courses/${id}`, true);
@@ -34,21 +39,56 @@ function getCourseInfo(id) {
     }
 }
 
-function getThisCourse(course) {
-    $("#course-chooser").hide(300);
-    renderHoles(course.data);
+function selectedTeeType(event) {
+    $(event.target.parentNode).animate({
+        opacity: 0
+    }, 300, function() {
+        event.target.parentNode.display = "none";
+        renderHoles();
+        $(".card-wrapper").hide();
+    })
 }
 
-function renderHoles(course) {
-    let html;
-    for(let hole in course.holes){
-        html += `
-            <div>
+function getThisCourse(course, id,  event) {
+    // $("#course-chooser").hide(300);
+    // renderHoles(course.data);
+    let teeTypeArray = course.data.holes[0].teeBoxes;
+    let teeType = "<option>Select A Tee Type</option>";
+    for(tee in teeTypeArray) {
+        teeType += `<option value="${teeTypeArray[tee].teeType}">${teeTypeArray[tee].teeType}</option>`;
+    }
+
+    document.getElementById(`select${id}`).innerHTML += teeType;
+
+    $(".card").slideUp(350);
+    $(event.target.parentNode).slideDown(350);
+
+    setTimeout(function() {
+        $(event.target).hide();
+        $(".card").hide();
+        $(event.target).parent().show();
+    }, 350);
+}
+
+function renderHoles() {
+    let firstNine;
+    for(let hole in selectedCourse.data.holes){
+            firstNine += `
+            <div id="hole${hole}">
                 <div class="hole-display">Hole</div>
-                <div>${course.holes[hole].hole}</div>
+                <div>${selectedCourse.data.holes[hole].hole}</div>
             </div>`;
     }
-    document.getElementById("hole-container").innerHTML = html;
+    document.getElementById("hole-container").innerHTML = firstNine;
+    document.getElementById("hole8").outerHTML += `<div id="out-label">
+                                                                <div>OUT</div>
+                                                            </div>`;
+    document.getElementById("hole17").outerHTML += `<div id="in-label">
+                                                                   <div>IN</div>
+                                                                </div>`;
+    document.getElementById("in-label").outerHTML += `<div>
+                                                                    <div id="total-label">TOTAL</div>
+                                                                </div>`
     document.getElementById("hole-container").firstChild.remove();
 }
 
@@ -60,9 +100,15 @@ function renderCourseList(APIcourse) {
         let courseId = APIcourse.courses[course].id;
         html += `<div class="card">
                     <div>${courseNameList}</div>
-                    <img style="width: 200px; height: 140px" src="${courseImage}"/>
-                    <button onclick="getCourseInfo(${courseId})">Select Tee Type</button>
+                    <img style="width: 350px; height: 250px" src="${courseImage}"/>
+                    <button onclick="getCourseInfo(${courseId}, event)">Select Tee Type</button>
+                    <select id="select${courseId}" onchange="selectedTeeType(event)" class="selectTee">
+                        
+                    </select>
                 </div>`;
     }
+
+
     $("#course-chooser").html(html);
 }
+
